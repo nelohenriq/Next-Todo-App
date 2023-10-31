74d9112db844ac85cd21964b9a368242d8ce1f6b
releaser <herminiohenriques@gmail.com>
2023-10-31 04:15:09 +0000

    clean setup

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,8 +2,13 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 export const authOptions = {
-  //adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
